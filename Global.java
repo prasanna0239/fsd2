6ef4c687b7952806a516ff70b8acbd19bf579ba5
prasanna0239 <23hr1a3026@gmail.com>
2025-09-13 11:26:25 +0530

    Add files via upload

---
+++ Global.java
@@ -0,0 +1,14 @@
+class Global {
+    public static void main(String args[])
+    {
+        for (int i = 0; i < 10; i++) {
+          
+            
+            if (i % 3 == 0)
+                continue;
+
+            
+            System.out.print(i + " ");
+        }
+    }
+}
